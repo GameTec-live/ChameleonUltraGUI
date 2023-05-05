7eb39f7734407c5ad402241798d802356f1fd890
GameTec_live <benedikt.werner08@gmail.com>
2023-05-05 21:26:51 +0200

    split into multiple files

---
+++ chameleonultragui/lib/serialcomBLE.dart
@@ -0,0 +1,9 @@
+import 'dart:io';
+import 'package:flutter_libserialport/flutter_libserialport.dart';
+
+class CommmoduleBLE {
+  // Class for BLE Serial Communication
+  SerialPort? port;
+  String? device;
+
+}
