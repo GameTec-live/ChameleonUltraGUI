e55a29e47a1c2f23078ceaf9628dde657abb25f6
GameTec_live <benedikt.werner08@gmail.com>
2023-06-02 20:11:19 +0200

    basic connect page structure, switching not implemented yet

---
+++ chameleonultragui/lib/gui/connectpage.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:logger/logger.dart';
+import '../main.dart';
+
+class ConnectPage extends StatelessWidget {
+  const ConnectPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>(); // Get State
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Connect'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center, // Center
+          children: [
+            Expanded(
+              child: GridView(
+                  padding: const EdgeInsets.all(20),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                    childAspectRatio: 1,
+                  ),
+                  scrollDirection: Axis.vertical,
+                  children: [
+                    /* "Add" Button for Bluetooth
+                    ElevatedButton(
+                      onPressed: () {
+                        appState.changesMade();
+                      },
+                      style: ButtonStyle(
+                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18.0),
+                          ),
+                        ),
+                      ),
+                      child: const Icon(Icons.add),
+                    ),
+                    */
+                    for (var i = 0; i < 10; i++)
+                      ElevatedButton(
+                        onPressed: () {
+                          appState.chameleon.connected = true;
+                          appState.changesMade();
+                        },
+                        style: ButtonStyle(
+                          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0),
+                            ),
+                          ),
+                        ),
+                        child: const Icon(Icons.add),
+                      ),
+                  ]
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
