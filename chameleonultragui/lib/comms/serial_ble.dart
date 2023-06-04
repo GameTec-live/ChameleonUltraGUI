b3e00ff6abdc60d7e18cf52802c71302ae3b96d3
GameTec_live <benedikt.werner08@gmail.com>
2023-06-04 09:09:01 +0200

    connect page progress

--- chameleonultragui/lib/comms/serial_ble.dart
+++ chameleonultragui/lib/comms/serial_ble.dart
@@ -3,3 +3,4 @@ import 'package:chameleonultragui/comms/serial_abstract.dart';
 class BLESerial extends AbstractSerial {
   // Class for BLE Serial Communication
 }
+// What bout that? https://pub.dev/packages/flutter_reactive_ble
