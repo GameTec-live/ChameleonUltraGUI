e7dc5c40795db2e959df316689a7fc50154ed9de
GameTec_live <benedikt.werner08@gmail.com>
2023-07-18 11:09:56 +0200

    homepage functionality

--- chameleonultragui/lib/gui/homepage.dart
+++ chameleonultragui/lib/gui/homepage.dart
@@ -12,7 +12,7 @@ class HomePage extends StatefulWidget {
 }
 class HomePageState extends State<HomePage> {
   late Future<(Icon, List<Icon>, String, String, String)> dataFuture;
-  var selectedSlot = 0;
+  var selectedSlot = 1;
 
   @override
   void initState() {
@@ -33,7 +33,7 @@ class HomePageState extends State<HomePage> {
   }
 
 
-  Future<Icon> getBatteryChargeIcon(connection) async {
+  Future<Icon> getBatteryChargeIcon(ChameleonCom connection) async {
     int charge = await connection.getBatteryCharge();
     if (charge > 98) {
       return const Icon(Icons.battery_full);
@@ -57,14 +57,14 @@ class HomePageState extends State<HomePage> {
     return const Icon(Icons.battery_unknown);
   }
 
-  Future<List<Icon>> getSlotIcons(connection, selectedSlot) async {
-    await connection.activateSlot(selectedSlot);
-    List<bool> usedSlots = await connection.getUsedSlots();
+  Future<List<Icon>> getSlotIcons(ChameleonCom connection, int selectedSlot) async {
+    connection.activateSlot(1);
+    List<bool> usedSlots = [false, false, false, false, false, false, false, false];
     List<Icon> icons = [];
     for (int i = 1; i < 9; i++) {
       if (i == selectedSlot) {
         icons.add(const Icon(Icons.circle_outlined, color: Colors.red,));
-      } else if (usedSlots[i]) {
+      } else if (usedSlots[i-1]) {
         icons.add(const Icon(Icons.circle));
       } else {
         icons.add(const Icon(Icons.circle_outlined));
@@ -73,8 +73,8 @@ class HomePageState extends State<HomePage> {
     return icons;
   }
 
-  Future<String> getUsedSlotsOut8(connection) async {
-    List<bool> usedSlots = await connection.getUsedSlots();
+  Future<String> getUsedSlotsOut8(ChameleonCom connection) async {
+    List<bool> usedSlots = [false, false, false, false, false, false, false, false];
     int usedSlotsOut8 = 0;
     for (int i = 0; i < 8; i++) {
       if (usedSlots[i]) {
@@ -84,11 +84,12 @@ class HomePageState extends State<HomePage> {
     return usedSlotsOut8.toString();
   }
 
-  Future<String> getFWversion(connection) async {
-    return await connection.getFirmwareVersion();
+  Future<String> getFWversion(ChameleonCom connection) async {
+    int fwv = await connection.getFirmwareVersion();
+    return fwv.toString();
   }
 
-  Future<String> getRamusage(connection) async {
+  Future<String> getRamusage(ChameleonCom connection) async {
     return await connection.getMemoryUsage();
   }
 
@@ -163,7 +164,7 @@ class HomePageState extends State<HomePage> {
                     children: [
                       IconButton(
                         onPressed: () {
-                          if (selectedSlot > 0) {
+                          if (selectedSlot > 1) {
                             selectedSlot--;
                           }
                           appState.changesMade();
@@ -173,7 +174,7 @@ class HomePageState extends State<HomePage> {
                       ...slotIcons,
                       IconButton(
                         onPressed: () {
-                          if (selectedSlot < 7) {
+                          if (selectedSlot < 8) {
                             selectedSlot++;
                           }
                           appState.changesMade();
