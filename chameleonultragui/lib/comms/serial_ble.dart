8ce8de7bf99a44153ff3d68aeb70a93d0189ba97
GameTec_live <benedikt.werner08@gmail.com>
2023-07-24 13:06:03 +0200

    ble available devices?

--- chameleonultragui/lib/comms/serial_ble.dart
+++ chameleonultragui/lib/comms/serial_ble.dart
@@ -2,7 +2,30 @@ import 'package:chameleonultragui/comms/serial_abstract.dart';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 
 class BLESerial extends AbstractSerial {
-  // Class for BLE Serial Communication
-}
+  
+  Future<List> availableDevicesBLE() async {
+    List<BluetoothDevice> devices = [];
 
+    // Start scanning for BLE devices
+    FlutterBluePlus.startScan(timeout: const Duration(seconds: 4), androidUsesFineLocation: false);
+
+    // Wait for scan to complete
+    await Future.delayed(const Duration(seconds: 4));
+
+    // Get a list of discovered devices
+    List<ScanResult> scanResults = await FlutterBluePlus.stopScan();
+
+    // Add each discovered device to the list
+    for (ScanResult result in scanResults) {
+      devices.add(result.device);
+    }
+
+    return devices;
+  }
+
+  Future<bool> connectDevice(BluetoothDevice device) async {
+    await device.connect();
+    return true;
+  }
+}
 // https://pub.dev/packages/flutter_blue_plus
