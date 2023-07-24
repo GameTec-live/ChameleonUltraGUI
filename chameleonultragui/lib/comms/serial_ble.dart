29d268152d0a52682b25e191514b7acafb820bf9
GameTec_live <benedikt.werner08@gmail.com>
2023-07-24 12:10:28 +0200

    add BLE library

--- chameleonultragui/lib/comms/serial_ble.dart
+++ chameleonultragui/lib/comms/serial_ble.dart
@@ -1,6 +1,8 @@
 import 'package:chameleonultragui/comms/serial_abstract.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 
 class BLESerial extends AbstractSerial {
   // Class for BLE Serial Communication
 }
-// What bout that? https://pub.dev/packages/flutter_reactive_ble
+
+// https://pub.dev/packages/flutter_blue_plus
--- chameleonultragui/macos/Flutter/GeneratedPluginRegistrant.swift
+++ chameleonultragui/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,6 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import file_saver
+import flutter_blue_plus
 import flutter_libserialport
 import path_provider_foundation
 import shared_preferences_foundation
@@ -13,6 +14,7 @@ import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSaverPlugin.register(with: registry.registrar(forPlugin: "FileSaverPlugin"))
+  FlutterBluePlusPlugin.register(with: registry.registrar(forPlugin: "FlutterBluePlusPlugin"))
   FlutterLibserialportPlugin.register(with: registry.registrar(forPlugin: "FlutterLibserialportPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
