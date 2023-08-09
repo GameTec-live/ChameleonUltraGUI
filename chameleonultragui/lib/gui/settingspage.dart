b94e08a5457f32ed9eea880f9745d5fe76a85f1d
GameTec_live <benedikt.werner08@gmail.com>
2023-08-09 19:28:09 +0200

    only fetch on about menu

--- chameleonultragui/lib/gui/settingspage.dart
+++ chameleonultragui/lib/gui/settingspage.dart
@@ -4,7 +4,7 @@ import 'package:toggle_switch/toggle_switch.dart';
 import 'package:logger/logger.dart';
 import 'package:chameleonultragui/helpers/opencollective.dart';
 import 'package:chameleonultragui/main.dart';
-
+//TODO: FIX INDENTING
 class SettingsMainPage extends StatefulWidget {
   const SettingsMainPage({Key? key}) : super(key: key);
 
@@ -36,114 +36,98 @@ class SettingsMainPageState extends State<SettingsMainPage> {
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>(); // Get State
 
-    return FutureBuilder(
-      future: getFutureData(),
-        builder: (BuildContext context, AsyncSnapshot snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Scaffold(
-              appBar: AppBar(
-                title: const Text('Settings'),
-              ),
-              body: const Center(child: CircularProgressIndicator()),
-            );
-          } else if (snapshot.hasError) {
-            return Text('Error: ${snapshot.error}');
-          } else {
-            final (
-              names
-            ) = snapshot.data;
-            return Scaffold(
-            appBar: AppBar(
-              title: const Text('Settings'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Settings'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center, // Center
+          children: [
+            Switch(
+              value: appState.switchOn,
+              activeColor: Colors.blue,
+              activeTrackColor: Colors.green,
+              inactiveThumbColor: Colors.white,
+              inactiveTrackColor: Colors.red,
+              onChanged: (bool value) {
+                // This is called when the user toggles the switch.
+                Logger log = Logger();
+                log.d('Switch toggled');
+                //appState.toggleswitch();
+                appState.switchOn = !appState.switchOn;
+                //(context as Element).reassemble();
+                appState.changesMade();
+              },
             ),
-            body: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center, // Center
-                children: [
-                  Switch(
-                    value: appState.switchOn,
-                    activeColor: Colors.blue,
-                    activeTrackColor: Colors.green,
-                    inactiveThumbColor: Colors.white,
-                    inactiveTrackColor: Colors.red,
-                    onChanged: (bool value) {
-                      // This is called when the user toggles the switch.
-                      Logger log = Logger();
-                      log.d('Switch toggled');
-                      //appState.toggleswitch();
-                      appState.switchOn = !appState.switchOn;
-                      //(context as Element).reassemble();
-                      appState.changesMade();
-                    },
-                  ),
-                  const SizedBox(height: 10),
-                  const Text("Sidebar Expansion:"),
-                  ToggleSwitch(
-                    minWidth: 90.0,
-                    cornerRadius: 20.0,
-                    activeFgColor: Colors.white,
-                    inactiveBgColor: Colors.grey,
-                    inactiveFgColor: Colors.white,
-                    initialLabelIndex:
-                        appState.sharedPreferencesProvider.getSideBarExpandedIndex(),
-                    totalSwitches: 3,
-                    labels: const ['Expand', 'automatic', 'retract'],
-                    radiusStyle: true,
-                    onToggle: (index) {
-                      if (index == 0) {
-                        appState.sharedPreferencesProvider.setSideBarExpanded(true);
-                        appState.sharedPreferencesProvider
-                            .setSideBarAutoExpansion(false);
-                      } else if (index == 2) {
-                        appState.sharedPreferencesProvider.setSideBarExpanded(false);
-                        appState.sharedPreferencesProvider
-                            .setSideBarAutoExpansion(false);
-                      } else {
-                        appState.sharedPreferencesProvider
-                            .setSideBarAutoExpansion(true);
+            const SizedBox(height: 10),
+            const Text("Sidebar Expansion:"),
+            ToggleSwitch(
+              minWidth: 90.0,
+              cornerRadius: 20.0,
+              activeFgColor: Colors.white,
+              inactiveBgColor: Colors.grey,
+              inactiveFgColor: Colors.white,
+              initialLabelIndex:
+                  appState.sharedPreferencesProvider.getSideBarExpandedIndex(),
+              totalSwitches: 3,
+              labels: const ['Expand', 'automatic', 'retract'],
+              radiusStyle: true,
+              onToggle: (index) {
+                if (index == 0) {
+                  appState.sharedPreferencesProvider.setSideBarExpanded(true);
+                  appState.sharedPreferencesProvider
+                      .setSideBarAutoExpansion(false);
+                } else if (index == 2) {
+                  appState.sharedPreferencesProvider.setSideBarExpanded(false);
+                  appState.sharedPreferencesProvider
+                      .setSideBarAutoExpansion(false);
+                } else {
+                  appState.sharedPreferencesProvider
+                      .setSideBarAutoExpansion(true);
+                }
+                appState.sharedPreferencesProvider
+                    .setSideBarExpandedIndex(index ?? 1);
+                appState.changesMade();
+              },
+            ),
+            const SizedBox(height: 10),
+            const Text("Theme:"),
+            ToggleSwitch(
+              minWidth: 90.0,
+              cornerRadius: 20.0,
+              activeFgColor: Colors.white,
+              inactiveBgColor: Colors.grey,
+              inactiveFgColor: Colors.white,
+              initialLabelIndex:
+              appState.sharedPreferencesProvider.getTheme() ==
+                            ThemeMode.system
+                        ? 0
+                        : appState.sharedPreferencesProvider.getTheme() ==
+                                ThemeMode.dark
+                            ? 2
+                            : 1,
+              totalSwitches: 3,
+              labels: const ['System', 'Light', 'Dark'],
+              radiusStyle: true,
+                  onToggle: (index) {
+                    if (index == 0) {
+                      appState.sharedPreferencesProvider.setTheme(ThemeMode.system);
+                    } else if (index == 2) {
+                      appState.sharedPreferencesProvider.setTheme(ThemeMode.dark);
+                    } else {
+                      appState.sharedPreferencesProvider.setTheme(ThemeMode.light);
                       }
-                      appState.sharedPreferencesProvider
-                          .setSideBarExpandedIndex(index ?? 1);
-                      appState.changesMade();
-                    },
+              appState.changesMade();
+              showDialog<String>(
+                context: context,
+                builder: (BuildContext context) => AlertDialog(
+                  title: const Text('Restart Required'),
+                  content: const Center(
+                    child: Text('Changes will take effect after a restart',
+                        style: TextStyle(fontWeight: FontWeight.bold)),
                   ),
-                  const SizedBox(height: 10),
-                  const Text("Theme:"),
-                  ToggleSwitch(
-                    minWidth: 90.0,
-                    cornerRadius: 20.0,
-                    activeFgColor: Colors.white,
-                    inactiveBgColor: Colors.grey,
-                    inactiveFgColor: Colors.white,
-                    initialLabelIndex:
-                        appState.sharedPreferencesProvider.getTheme() ==
-                                ThemeMode.system
-                            ? 0
-                            : appState.sharedPreferencesProvider.getTheme() ==
-                                    ThemeMode.dark
-                                ? 2
-                                : 1,
-                    totalSwitches: 3,
-                    labels: const ['System', 'Light', 'Dark'],
-                    radiusStyle: true,
-                    onToggle: (index) {
-                      if (index == 0) {
-                        appState.sharedPreferencesProvider.setTheme(ThemeMode.system);
-                      } else if (index == 2) {
-                        appState.sharedPreferencesProvider.setTheme(ThemeMode.dark);
-                      } else {
-                        appState.sharedPreferencesProvider.setTheme(ThemeMode.light);
-                      }
-                      appState.changesMade();
-                      showDialog<String>(
-                        context: context,
-                        builder: (BuildContext context) => AlertDialog(
-                          title: const Text('Restart Required'),
-                          content: const Center(
-                            child: Text('Changes will take effect after a restart',
-                                style: TextStyle(fontWeight: FontWeight.bold)),
-                          ),
-                          actions: <Widget>[
+                  actions: <Widget>[
                             TextButton(
                               onPressed: () => Navigator.pop(context, 'OK'),
                               child: const Text('OK'),
@@ -223,32 +207,46 @@ class SettingsMainPageState extends State<SettingsMainPage> {
                       builder: (BuildContext context) => AlertDialog(
                         title: const Text('About'),
                         content: Center(
-                          child: Column(
-                            children: [
-                              const Text('Chameleon Ultra GUI',
-                                  style: TextStyle(fontWeight: FontWeight.bold)),
-                              const Text(
-                                  'A Tool to graphically manage and configure your Chameleon Ultra, written in Flutter and running on Desktop and Mobile.'),
-                              const SizedBox(height: 10),
-                              const Text('Version:'),
-                              const Text('UNRELEASED',
-                                  style: TextStyle(fontWeight: FontWeight.bold)),
-                              const SizedBox(height: 10),
-                              const Text('Developed by:'),
-                              const Text('Foxushka, Akisame and GameTec_live',
-                                  style: TextStyle(fontWeight: FontWeight.bold)),
-                              const SizedBox(height: 10),
-                              const Text('License:'),
-                              const Text('GPLV3',
-                                  style: TextStyle(fontWeight: FontWeight.bold)),
-                              const SizedBox(height: 10),
-                              const Text(
-                                  'https://github.com/GameTec-live/ChameleonUltraGUI'),
-                              const SizedBox(height: 30),
-                              const Text("Thanks to everyone who supports us on Open Collective!"),
-                              const SizedBox(height: 10),
-                              Text(names, style: const TextStyle(fontWeight: FontWeight.bold)),
-                            ],
+                          child: FutureBuilder(
+                            future: getFutureData(),
+                            builder: (BuildContext context, AsyncSnapshot snapshot) {
+                              if (snapshot.connectionState == ConnectionState.waiting) {
+                                return const Center(child: CircularProgressIndicator());
+                              } else if (snapshot.hasError) {
+                                return Text('Error: ${snapshot.error}');
+                              } else {
+                                final (
+                                  names
+                                ) = snapshot.data;
+                                return Column(
+                              children: [
+                                const Text('Chameleon Ultra GUI',
+                                    style: TextStyle(fontWeight: FontWeight.bold)),
+                                const Text(
+                                    'A Tool to graphically manage and configure your Chameleon Ultra, written in Flutter and running on Desktop and Mobile.'),
+                                const SizedBox(height: 10),
+                                const Text('Version:'),
+                                const Text('UNRELEASED',
+                                    style: TextStyle(fontWeight: FontWeight.bold)),
+                                const SizedBox(height: 10),
+                                const Text('Developed by:'),
+                                const Text('Foxushka, Akisame and GameTec_live',
+                                    style: TextStyle(fontWeight: FontWeight.bold)),
+                                const SizedBox(height: 10),
+                                const Text('License:'),
+                                const Text('GPLV3',
+                                    style: TextStyle(fontWeight: FontWeight.bold)),
+                                const SizedBox(height: 10),
+                                const Text(
+                                    'https://github.com/GameTec-live/ChameleonUltraGUI'),
+                                const SizedBox(height: 30),
+                                const Text("Thanks to everyone who supports us on Open Collective!"),
+                                const SizedBox(height: 10),
+                                Text(names, style: const TextStyle(fontWeight: FontWeight.bold)),
+                              ],
+                            );
+                            }
+                            },
                           ),
                         ),
                         actions: <Widget>[
@@ -300,13 +298,10 @@ class SettingsMainPageState extends State<SettingsMainPage> {
                       ),
                     ),
                     child: const Text('Activate developer mode'),
-                  )
-                ],
-              ),
-            ),
-          );
-        }
-      }
+            )
+          ],
+        ),
+      ),
     );
   }
 }
