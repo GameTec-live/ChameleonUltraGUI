c57fda6a9d6825d529dd32de5c1ef150b777a056
Foxushka <135865149+Foxushka@users.noreply.github.com>
2023-08-15 10:47:33 +0000

    Fix naming (#108)
    * Fix slot name fetching
    
    * Fix #97

--- chameleonultragui/lib/gui/connectpage.dart
+++ chameleonultragui/lib/gui/connectpage.dart
@@ -56,31 +56,6 @@ class ConnectPage extends StatelessWidget {
                         ),
                         scrollDirection: Axis.vertical,
                         children: [
-                          // "Add" Button for Bluetooth
-                          ElevatedButton(
-                            onPressed: () {
-                              showDialog<String>(
-                                context: context,
-                                builder: (BuildContext context) =>
-                                    const AlertDialog(
-                                  content: Text('Identifies as BLE Dialog'),
-                                ),
-                              );
-                              // Connect via BLE here
-                              appState.connector.connected =
-                                  true; // Bypass / Dummy for testing
-                              appState.changesMade();
-                            },
-                            style: ButtonStyle(
-                              shape: MaterialStateProperty.all<
-                                  RoundedRectangleBorder>(
-                                RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(18.0),
-                                ),
-                              ),
-                            ),
-                            child: const Icon(Icons.add),
-                          ),
                           ...result.map<Widget>((chameleonDevice) {
                             return ElevatedButton(
                               onPressed: () async {
@@ -149,41 +124,53 @@ class ConnectPage extends StatelessWidget {
                                 children: [
                                   Padding(
                                     padding: const EdgeInsets.all(8.0),
-                                    child: Row(
-                                      mainAxisAlignment: MainAxisAlignment.end,
-                                      children: [
-                                        const Icon(Icons.usb),
-                                        Text(chameleonDevice['port'] ??
-                                            "MISSING"),
-                                        if (chameleonDevice['type'] ==
-                                            ChameleonConnectType.dfu)
-                                          const Text(" (DFU)"),
-                                      ],
+                                    child: FittedBox(
+                                      alignment: Alignment.centerRight,
+                                      fit: BoxFit.scaleDown,
+                                      child: Row(
+                                        mainAxisAlignment: MainAxisAlignment
+                                            .end, // Align the inner Row's children to the right
+                                        children: [
+                                          Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.end,
+                                            children: [
+                                              const Icon(Icons.usb),
+                                              Text(chameleonDevice['port'] ??
+                                                  ""),
+                                              if (chameleonDevice['type'] ==
+                                                  ChameleonConnectType.dfu)
+                                                const Text(" (DFU)"),
+                                            ],
+                                          )
+                                        ],
+                                      ),
                                     ),
                                   ),
-                                  Row(
-                                    mainAxisAlignment: MainAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                          "Chameleon ${(chameleonDevice['device'] == ChameleonDevice.ultra) ? 'Ultra' : 'Lite'}",
-                                          style: const TextStyle(
-                                              fontWeight: FontWeight.bold,
-                                              fontSize: 20)),
-                                    ],
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.only(top: 8.0),
-                                    child: FractionallySizedBox(
-                                      widthFactor: 0.4,
+                                  FittedBox(
+                                      alignment: Alignment.topRight,
+                                      fit: BoxFit.scaleDown,
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                              "Chameleon ${(chameleonDevice['device'] == ChameleonDevice.ultra) ? 'Ultra' : 'Lite'}",
+                                              style: const TextStyle(
+                                                  fontWeight: FontWeight.bold,
+                                                  fontSize: 20)),
+                                        ],
+                                      )),
+                                  const SizedBox(height: 8),
+                                  Expanded(
+                                      flex: 1,
                                       child: Image.asset(
                                         chameleonDevice['device'] ==
                                                 ChameleonDevice.ultra
                                             ? 'assets/black-ultra-standing-front.png'
                                             : 'assets/black-lite-standing-front.png',
-                                        fit: BoxFit.contain,
-                                      ),
-                                    ),
-                                  ),
+                                        fit: BoxFit.fitHeight,
+                                      )),
                                 ],
                               ),
                             );
