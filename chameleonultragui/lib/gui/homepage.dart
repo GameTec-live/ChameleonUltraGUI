19df41c98b2d452868c0250f1bf1f32977978912
GameTec_live <benedikt.werner08@gmail.com>
2023-08-18 20:30:51 +0200

    only refresh parts that are required to refresh

--- chameleonultragui/lib/gui/homepage.dart
+++ chameleonultragui/lib/gui/homepage.dart
@@ -264,33 +264,7 @@ class HomePageState extends State<HomePage> {
                     Text("Used Slots: $usedSlots/8",
                         style: TextStyle(
                             fontSize: MediaQuery.of(context).size.width / 50)),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        IconButton(
-                          onPressed: () async {
-                            if (selectedSlot > 1) {
-                              await connection.activateSlot(selectedSlot - 2);
-                              setState(() {});
-                              appState.changesMade();
-                            }
-                          },
-                          icon: const Icon(Icons.arrow_back),
-                        ),
-                        ...slotIcons,
-                        IconButton(
-                          onPressed: () async {
-                            if (selectedSlot < 8) {
-                              await connection
-                                  .activateSlot(selectedSlot); // already +1
-                              setState(() {});
-                              appState.changesMade();
-                            }
-                          },
-                          icon: const Icon(Icons.arrow_forward),
-                        ),
-                      ],
-                    ),
+                    SlotChanger(),
                     Expanded(
                       child: FractionallySizedBox(
                         widthFactor: 0.4,
@@ -608,6 +582,108 @@ class HomePageState extends State<HomePage> {
               ),
             );
           }
-        });
+        }
+      );
+  }
+}
+
+
+
+class SlotChanger extends StatefulWidget {
+  const SlotChanger({Key? key}) : super(key: key);
+
+  @override
+  SlotChangerState createState() => SlotChangerState();
+}
+
+class SlotChangerState extends State<SlotChanger> {
+  var selectedSlot = 1;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Future<List<Icon>> getFutureData() async {
+    var appState = context.read<MyAppState>();
+    var connection = ChameleonCom(port: appState.connector);
+    List<(ChameleonTag, ChameleonTag)> usedSlots;
+    try {
+      usedSlots = await connection.getUsedSlots();
+    } catch (_) {
+      usedSlots = [];
+    }
+
+    return await getSlotIcons(connection, usedSlots);
+  }
+
+  Future<List<Icon>> getSlotIcons(ChameleonCom connection,
+      List<(ChameleonTag, ChameleonTag)> usedSlots) async {
+    List<Icon> icons = [];
+    try {
+      selectedSlot = await connection.getActiveSlot() + 1;
+    } catch (_) {
+      selectedSlot = 1;
+    }
+    for (int i = 1; i < 9; i++) {
+      if (i == selectedSlot) {
+        icons.add(const Icon(
+          Icons.circle_outlined,
+          color: Colors.red,
+        ));
+      } else if (false) {
+        icons.add(const Icon(Icons.circle));
+      } else {
+        icons.add(const Icon(Icons.circle_outlined));
+      }
+    }
+    return icons;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.read<MyAppState>();
+    var connection = ChameleonCom(port: appState.connector);
+
+    return FutureBuilder(
+      future: getFutureData(),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          appState.connector.preformDisconnect();
+          return Text('Error: ${snapshot.error.toString()}');
+        } else {
+          final slotIcons = snapshot.data;
+
+          return Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(
+                onPressed: () async {
+                  if (selectedSlot > 1) {
+                    await connection.activateSlot(selectedSlot - 2);
+                    setState(() {});
+                    appState.changesMade();
+                  }
+                },
+                icon: const Icon(Icons.arrow_back),
+              ),
+              ...slotIcons,
+              IconButton(
+                onPressed: () async {
+                  if (selectedSlot < 8) {
+                    await connection.activateSlot(selectedSlot);
+                    setState(() {});
+                    appState.changesMade();
+                  }
+                },
+                icon: const Icon(Icons.arrow_forward),
+              ),
+            ],
+          );
+        }
+      }
+    );
   }
 }
