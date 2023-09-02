11f5d4b511366ae5d86fbb9558558e0204fd65c6
Foxushka <135865149+Foxushka@users.noreply.github.com>
2023-09-02 11:15:18 +0000

    Create update.py

---
+++ chameleonultragui/lib/l10n/update.py
@@ -0,0 +1,42 @@
+import json
+import os
+import sys
+import urllib.error
+from urllib.request import Request, urlopen
+
+
+def progressbar(it, prefix="", size=60, out=sys.stdout):
+    count = len(it)
+
+    def show(j):
+        x = int(size * j / count)
+        print(f"{prefix}[{u'█' * x}{('.' * (size - x))}] {j}/{count}", end='\r', file=out, flush=True)
+
+    show(0)
+    for i, item in enumerate(it):
+        yield item
+        show(i + 1)
+    print("\n", flush=True, file=out)
+
+
+def request(method, url, data=None):
+    if not data:
+        data = {}
+    return json.loads(urlopen(Request(url, method=method, data=json.dumps(data).encode(),
+                                      headers={'Accept': 'application/json',
+                                               'Authorization': 'Bearer ' + os.getenv('CROWDIN_API'),
+                                               'Content-Type': 'application/json'})).read().decode())
+
+
+for language in progressbar(request('GET', 'https://crowdin.com/api/v2/languages?limit=500')['data']):
+    try:
+        progress = request('GET',
+                           f'https://crowdin.com/api/v2/projects/610545/languages/{language["data"]["id"]}/progress')
+    except urllib.error.HTTPError:
+        continue
+    if progress['data'][0]['data']['words']['translated']:
+        translation = request("POST", "https://crowdin.com/api/v2/projects/610545/translations/exports",
+                              {"targetLanguageId": language["data"]["id"], "format": "arb-export",
+                               "skipUntranslatedStrings": True, "fileIds": [9]})
+        export = urlopen(Request(translation['data']['url'], method='GET')).read()
+        open(f"app_{language['data']['osxLocale']}.arb", "wb+").write(export)
