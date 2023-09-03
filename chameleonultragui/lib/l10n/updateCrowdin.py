baa764e050303beeb684244ac3ac87e89d85292e
Andrés Ruz Nieto - MAC <40019177+aruznieto@users.noreply.github.com>
2023-09-03 19:16:01 +0200

    Crowdin Add Key Automation

---
+++ chameleonultragui/lib/l10n/updateCrowdin.py
@@ -0,0 +1,58 @@
+import sys
+import os
+import json
+from urllib.request import Request, urlopen
+
+api_token = os.getenv('CROWDIN_API')
+projectId =  611911
+sourceId = 33
+
+def progressbar(it, prefix='', size=60, out=sys.stdout):
+    count = len(it)
+
+    def show(j):
+        x = int(size * j / count)
+        print(f"{prefix}[{u'█' * x}{('.' * (size - x))}] {j}/{count}", end='\r', file=out, flush=True)
+
+    show(0)
+    for i, item in enumerate(it):
+        yield item
+        show(i + 1)
+    print('\n', flush=True, file=out)
+
+
+def request(method, url, data=None):
+    if not data:
+        data = {}
+    return json.loads(urlopen(Request(url, method=method, data=json.dumps(data).encode(),
+                                      headers={'Accept': 'application/json',
+                                               'Authorization': 'Bearer ' + api_token,
+                                               'Content-Type': 'application/json'})).read().decode())
+
+def add_string_to_source(string, context=None, type='text'):
+
+    headers={'Accept': 'application/json',
+             'Authorization': 'Bearer ' + api_token,
+             'Content-Type': 'application/json'}
+    
+    try:
+        string_parsed = string.split('\n', 1)[1].replace(' ', '').replace(',', '').replace('\n','').split('+')[1:]
+        for s in string_parsed:
+            data = {'identifier': key.replace('"', ''), 'text': value.replace('"', ''), 'fileId': sourceId}
+            try:
+                key, value = s.split(':')
+                request('POST', f"https://api.crowdin.com/api/v2/projects/{projectId}/strings", data)
+                print("Added: ", key)
+            except:
+                print("This string already exist: ", key)
+
+    except:
+        print("Error adding:", string)
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Uso: python script.py <cadena_a_agregar>")
+        sys.exit(1)
+
+    string_to_be_added = sys.argv[1]
+    add_string_to_source(string_to_be_added)
