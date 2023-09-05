23b215e36091169ba6333d474e5b3bcfbc67270d
GameTec_live <benedikt.werner08@gmail.com>
2023-09-05 07:59:04 +0200

    replace google fonts with local fonts

--- chameleonultragui/macos/Flutter/GeneratedPluginRegistrant.swift
+++ chameleonultragui/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,7 +10,6 @@ import flutter_libserialport
 import package_info_plus
 import path_provider_foundation
 import shared_preferences_foundation
-import sqflite
 import url_launcher_macos
 import wakelock_plus
 
@@ -20,7 +19,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   WakelockPlusMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockPlusMacosPlugin"))
 }
