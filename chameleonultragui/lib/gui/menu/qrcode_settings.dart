2fe9fc13260a92791f1d7a51a6dfb01a4aecdc66
GameTec_live <benedikt.werner08@gmail.com>
2023-11-24 13:16:54 +0100

    fix: eh, itll be fine...

--- chameleonultragui/lib/gui/menu/qrcode_settings.dart
+++ chameleonultragui/lib/gui/menu/qrcode_settings.dart
@@ -39,144 +39,136 @@ class QRCodeSettingsState extends State<QRCodeSettings> {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              Row(
-                children: [
-                  Slider(
-                    value: sliderSplitSize.toDouble(),
-                    onChanged: (double value) {
-                      if (sliderErrorCorrection.toInt() == 2 && value > 1200) {
-                        sliderErrorCorrection = 3;
-                      } else if (sliderErrorCorrection.toInt() == 3 && value > 1600) {
-                        sliderErrorCorrection = 0;
-                      }
-        
-                      setState(() {
-                        sliderErrorCorrection = sliderErrorCorrection;
-                        errorCorrection.text = sliderErrorCorrection.toString();
-                        sliderSplitSize = value.toInt();
-                        splitSize.text = value.toInt().toString();
-                      });
-                    },
-                    min: 1,
-                    max: 2048,
-                  ),
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width *
-                        0.08, //TODO: Constrain properly
-                    child: TextFormField(
-                      controller: splitSize,
-                      decoration: InputDecoration(
-                        labelText: AppLocalizations.of(context)!.split_size,
-                        hintText: "2048",
-                        suffix: Tooltip(
-                          message:
-                              AppLocalizations.of(context)!.split_size_tooltip,
-                          child: const Icon(Icons.info_outline),
-                        ),
-                      ),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return AppLocalizations.of(context)!
-                              .please_enter_something("Split Size");
-                        }
-                        if (int.tryParse(value) == null) {
-                          return AppLocalizations.of(context)!
-                              .please_enter_a_valid_number;
-                        }
-                        if (int.tryParse(value)! < 1) {
-                          return AppLocalizations.of(context)!
-                              .please_enter_a_number_greater_than("0");
-                        }
-                        if (int.tryParse(value)! > 2048) {
-                          return AppLocalizations.of(context)!
-                              .please_enter_a_valid_number;
-                        }
-                        return null;
-                      },
+              Slider(
+                value: sliderSplitSize.toDouble(),
+                onChanged: (double value) {
+                  if (sliderErrorCorrection.toInt() == 2 && value > 1200) {
+                    sliderErrorCorrection = 3;
+                  } else if (sliderErrorCorrection.toInt() == 3 &&
+                      value > 1600) {
+                    sliderErrorCorrection = 0;
+                  }
+
+                  setState(() {
+                    sliderErrorCorrection = sliderErrorCorrection;
+                    errorCorrection.text = sliderErrorCorrection.toString();
+                    sliderSplitSize = value.toInt();
+                    splitSize.text = value.toInt().toString();
+                  });
+                },
+                min: 1,
+                max: 2048,
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: TextFormField(
+                  controller: splitSize,
+                  decoration: InputDecoration(
+                    labelText: AppLocalizations.of(context)!.split_size,
+                    hintText: "2048",
+                    suffix: Tooltip(
+                      message: AppLocalizations.of(context)!.split_size_tooltip,
+                      child: const Icon(Icons.info_outline),
                     ),
                   ),
-                ],
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return AppLocalizations.of(context)!
+                          .please_enter_something("Split Size");
+                    }
+                    if (int.tryParse(value) == null) {
+                      return AppLocalizations.of(context)!
+                          .please_enter_a_valid_number;
+                    }
+                    if (int.tryParse(value)! < 1) {
+                      return AppLocalizations.of(context)!
+                          .please_enter_a_number_greater_than("0");
+                    }
+                    if (int.tryParse(value)! > 2048) {
+                      return AppLocalizations.of(context)!
+                          .please_enter_a_valid_number;
+                    }
+                    return null;
+                  },
+                ),
               ),
-              Row(
-                children: [
-                  Slider(
-                    value: sliderErrorCorrection.toDouble(),
-                    onChanged: (double value) {
-                      if (value.toInt() == 0 && sliderSplitSize > 2048) {
-                        sliderSplitSize = 2048;
-                      } else if (value.toInt() == 1 && sliderSplitSize > 2048) {
-                        sliderSplitSize = 2048;
-                      } else if (value.toInt() == 2 && sliderSplitSize > 1200) {
-                        sliderSplitSize = 1200;
-                      } else if (value.toInt() == 3 && sliderSplitSize > 1600) {
-                        sliderSplitSize = 1600;
-                      }
-                      setState(() {
-                        sliderSplitSize = sliderSplitSize;
-                        splitSize.text = sliderSplitSize.toString();
-                        sliderErrorCorrection = value.toInt();
-                        errorCorrection.text = value.toInt().toString();
-                      });
-                    },
-                    min: 0,
-                    max: 3,
-                  ),
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width *
-                        0.08, //TODO: Constrain properly
-                    child: TextFormField(
-                      controller: errorCorrection,
-                      decoration: InputDecoration(
-                        labelText: AppLocalizations.of(context)!.error_correction,
-                        hintText: "0",
-                        suffix: Tooltip(
-                          message: AppLocalizations.of(context)!
-                              .error_correction_tooltip,
-                          child: const Icon(Icons.info_outline),
-                        ),
-                      ),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return AppLocalizations.of(context)!
-                              .please_enter_something("Error Correction");
-                        }
-                        if (int.tryParse(value) == null) {
-                          return AppLocalizations.of(context)!
-                              .please_enter_a_valid_number;
-                        }
-                        if (int.tryParse(value)! < 0 ||
-                            int.tryParse(value)! > 3) {
-                          return AppLocalizations.of(context)!
-                              .please_enter_a_number_between("0", "3");
-                        }
-                        return null;
-                      },
+              const SizedBox(height: 50),
+              Slider(
+                value: sliderErrorCorrection.toDouble(),
+                onChanged: (double value) {
+                  if (value.toInt() == 0 && sliderSplitSize > 2048) {
+                    sliderSplitSize = 2048;
+                  } else if (value.toInt() == 1 && sliderSplitSize > 2048) {
+                    sliderSplitSize = 2048;
+                  } else if (value.toInt() == 2 && sliderSplitSize > 1200) {
+                    sliderSplitSize = 1200;
+                  } else if (value.toInt() == 3 && sliderSplitSize > 1600) {
+                    sliderSplitSize = 1600;
+                  }
+                  setState(() {
+                    sliderSplitSize = sliderSplitSize;
+                    splitSize.text = sliderSplitSize.toString();
+                    sliderErrorCorrection = value.toInt();
+                    errorCorrection.text = value.toInt().toString();
+                  });
+                },
+                min: 0,
+                max: 3,
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: TextFormField(
+                  controller: errorCorrection,
+                  decoration: InputDecoration(
+                    labelText: AppLocalizations.of(context)!.error_correction,
+                    hintText: "0",
+                    suffix: Tooltip(
+                      message: AppLocalizations.of(context)!
+                          .error_correction_tooltip,
+                      child: const Icon(Icons.info_outline),
                     ),
                   ),
-                ],
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return AppLocalizations.of(context)!
+                          .please_enter_something("Error Correction");
+                    }
+                    if (int.tryParse(value) == null) {
+                      return AppLocalizations.of(context)!
+                          .please_enter_a_valid_number;
+                    }
+                    if (int.tryParse(value)! < 0 || int.tryParse(value)! > 3) {
+                      return AppLocalizations.of(context)!
+                          .please_enter_a_number_between("0", "3");
+                    }
+                    return null;
+                  },
+                ),
               ),
-              const SizedBox(height: 10),
-              Align(alignment: Alignment.centerLeft, child: Text(AppLocalizations.of(context)!.test_qr_code)),
+              const SizedBox(height: 50),
+              Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(AppLocalizations.of(context)!.test_qr_code)),
               const SizedBox(height: 10),
               SizedBox(
-                  width: MediaQuery.of(context).size.width >
-                          MediaQuery.of(context).size.height
-                      ? MediaQuery.of(context).size.height * 0.8
-                      : MediaQuery.of(context).size.width * 0.8,
-                  height: MediaQuery.of(context).size.width >
-                          MediaQuery.of(context).size.height
-                      ? MediaQuery.of(context).size.height * 0.8
-                      : MediaQuery.of(context).size.width * 0.8,
-                  child: QrImageView(
-                    // Generate dummy data on the fly depending on the splitsize
-                    data: List.filled(sliderSplitSize, "a").join(""),
-                    version: QrVersions.auto,
-                    size: 200.0,
-                    backgroundColor: Colors.white,
-                    padding: const EdgeInsets.all(20.0),
-                    errorCorrectionLevel: sliderErrorCorrection,
-                  ),
+                width: MediaQuery.of(context).size.width >
+                        MediaQuery.of(context).size.height
+                    ? MediaQuery.of(context).size.height * 0.8
+                    : MediaQuery.of(context).size.width * 0.8,
+                height: MediaQuery.of(context).size.width >
+                        MediaQuery.of(context).size.height
+                    ? MediaQuery.of(context).size.height * 0.8
+                    : MediaQuery.of(context).size.width * 0.8,
+                child: QrImageView(
+                  // Generate dummy data on the fly depending on the splitsize
+                  data: List.filled(sliderSplitSize, "a").join(""),
+                  version: QrVersions.auto,
+                  size: 200.0,
+                  backgroundColor: Colors.white,
+                  padding: const EdgeInsets.all(20.0),
+                  errorCorrectionLevel: sliderErrorCorrection,
                 ),
+              ),
             ],
           ),
         ),
