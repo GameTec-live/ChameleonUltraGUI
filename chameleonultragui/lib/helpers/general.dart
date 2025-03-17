5a5e3b74121d67a2db5f49c74e20a01227377007
Foxushka <135865149+Foxushka@users.noreply.github.com>
2025-03-17 20:18:48 +0300

    fix: Fix Android calling checkPermissions multiple times at once
    Fixes error on first app launch when no permissions were provided

--- chameleonultragui/lib/helpers/general.dart
+++ chameleonultragui/lib/helpers/general.dart
@@ -211,7 +211,7 @@ TagType getTagTypeByValue(int value) {
 }
 
 String colorToHex(Color color) {
-  return '#${color.value.toRadixString(16).padLeft(8, '0').substring(2)}';
+  return '#${color.toARGB32().toRadixString(16).padLeft(8, '0').substring(2)}';
 }
 
 Color hexToColor(String hex) {
