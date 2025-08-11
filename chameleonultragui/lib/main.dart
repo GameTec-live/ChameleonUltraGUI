30691affafcea122d5560288a5d6d8ea56800d0c
Foxushka <135865149+Foxushka@users.noreply.github.com>
2025-08-11 17:12:21 +0300

    Update main.dart

--- chameleonultragui/lib/main.dart
+++ chameleonultragui/lib/main.dart
@@ -247,7 +247,7 @@ class _MainPageState extends State<MainPage> {
                     .surface,
                 statusBarBrightness: Brightness.light,
                 statusBarIconBrightness: Brightness.dark)),
-      ),
+      ).useCustomSystemFont(Brightness.light),
       darkTheme: ThemeData(
         useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(
@@ -263,7 +263,7 @@ class _MainPageState extends State<MainPage> {
                     .surface,
                 statusBarBrightness: Brightness.dark,
                 statusBarIconBrightness: Brightness.light)),
-      ),
+      ).useCustomSystemFont(Brightness.dark),
       themeMode: widget.sharedPreferencesProvider.getTheme(), // Dark Theme
       home: LayoutBuilder(// Build Page
           builder: (context, constraints) {
