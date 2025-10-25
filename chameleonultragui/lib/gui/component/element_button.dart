7c371e9b3c9a16b4a11265ea03e4d8018c5f472b
Foxushka <135865149+Foxushka@users.noreply.github.com>
2025-10-25 21:28:56 +0300

    fix: Fix icon position again

--- chameleonultragui/lib/gui/component/element_button.dart
+++ chameleonultragui/lib/gui/component/element_button.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:chameleonultragui/main.dart';
 import 'package:chameleonultragui/gui/component/card_button.dart';
 import 'package:flutter/material.dart';
@@ -29,92 +31,120 @@ class ElementButton extends StatefulWidget {
 }
 
 class ElementButtonState extends State<ElementButton> {
+  static const double _iconSize = 32.0;
+  static const double _iconSpacing = 8.0;
+  static const double _iconTransformX = -5.0;
+
   @override
   void initState() {
     super.initState();
   }
 
-  bool shouldMoveIcons() {
-    var appState = context.read<ChameleonGUIState>();
+  double _getIconButtonWidth(BuildContext context) {
+    final MaterialTapTargetSize tapTargetSize =
+        Theme.of(context).materialTapTargetSize;
 
-    double readWidth = MediaQuery.of(context).size.width;
-    double predictedElementWidth = readWidth >= 700
-        ? (readWidth - appState.navigationRailSize!.width - 38) / 2
-        : (readWidth - appState.navigationRailSize!.width - 28);
-    double iconsWidth = widget.children.length * 40 + 8;
-    double textWidth = (TextPainter(
-            text: TextSpan(
-                text: widget.secondLine.length > widget.firstLine.length
-                    ? widget.secondLine
-                    : widget.firstLine),
-            maxLines: 1,
-            textScaler: MediaQuery.of(context).textScaler,
-            textDirection: TextDirection.ltr)
-          ..layout())
-        .size
-        .width;
-    double textEndsAt = textWidth + 90;
+    return tapTargetSize == MaterialTapTargetSize.padded ? 48.0 : 40.0;
+  }
 
-    return textEndsAt + iconsWidth > predictedElementWidth &&
-        widget.children.isNotEmpty;
+  bool _shouldMoveIcons(BuildContext context, double availableWidth) {
+    if (widget.children.isEmpty) return false;
+
+    double iconButtonWidth = _getIconButtonWidth(context);
+    double iconsWidth = widget.children.length * iconButtonWidth + 8;
+
+    TextStyle textStyle =
+        Theme.of(context).textTheme.bodyMedium ?? const TextStyle();
+
+    TextPainter firstLinePainter = TextPainter(
+      text: TextSpan(text: widget.firstLine, style: textStyle),
+      maxLines: 1,
+      textScaler: MediaQuery.of(context).textScaler,
+      textDirection: TextDirection.ltr,
+    )..layout();
+
+    TextPainter secondLinePainter = TextPainter(
+      text: TextSpan(text: widget.secondLine, style: textStyle),
+      maxLines: 1,
+      textScaler: MediaQuery.of(context).textScaler,
+      textDirection: TextDirection.ltr,
+    )..layout();
+
+    double textWidth =
+        max(firstLinePainter.size.width, secondLinePainter.size.width);
+
+    double horizontalSpacing = _iconSize + _iconSpacing + _iconTransformX;
+
+    double contentWidthNeeded = horizontalSpacing + textWidth;
+    double totalWidthNeeded = contentWidthNeeded + iconsWidth;
+
+    return totalWidthNeeded > availableWidth;
   }
 
   @override
   Widget build(BuildContext context) {
     var appState = context.read<ChameleonGUIState>();
 
-    return Container(
-        constraints: BoxConstraints(maxHeight: !shouldMoveIcons() ? 90 : 130),
-        child: ElevatedButton(
-            onPressed: widget.onPressed,
-            style: customCardButtonStyle(appState),
-            child: Padding(
-              padding: const EdgeInsets.only(top: 8.0, left: 8.0, bottom: 6.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Transform(
-                        transform: Matrix4.translationValues(-5, 0, 0),
-                        child: Icon(
-                          widget.icon,
-                          color: widget.iconColor,
-                          size: 32,
-                        ),
-                      ),
-                      const SizedBox(width: 8),
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              widget.firstLine,
-                              maxLines: widget.maxLineLines,
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                            const SizedBox(height: 4),
-                            Text(
-                              widget.secondLine,
-                              maxLines: widget.maxLineLines,
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                          ],
-                        ),
-                      ),
-                      if (!shouldMoveIcons()) ...widget.children
-                    ],
-                  ),
-                  if (shouldMoveIcons()) ...[
-                    const SizedBox(height: 10),
+    return ElevatedButton(
+        onPressed: widget.onPressed,
+        style: customCardButtonStyle(appState),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 8.0, left: 8.0, bottom: 6.0),
+          child: LayoutBuilder(
+            builder: (context, constraints) {
+              bool moveIcons = _shouldMoveIcons(context, constraints.maxWidth);
+
+              return ConstrainedBox(
+                constraints: BoxConstraints(maxHeight: !moveIcons ? 90 : 130),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
                     Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: widget.children,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Transform(
+                          transform:
+                              Matrix4.translationValues(_iconTransformX, 0, 0),
+                          child: Icon(
+                            widget.icon,
+                            color: widget.iconColor,
+                            size: _iconSize,
+                          ),
+                        ),
+                        const SizedBox(width: _iconSpacing),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                widget.firstLine,
+                                maxLines: widget.maxLineLines,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                              const SizedBox(height: 4),
+                              Text(
+                                widget.secondLine,
+                                maxLines: widget.maxLineLines,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ],
+                          ),
+                        ),
+                        if (!moveIcons) ...widget.children
+                      ],
                     ),
-                  ]
-                ],
-              ),
-            )));
+                    if (moveIcons) ...[
+                      const SizedBox(height: 10),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: widget.children,
+                      ),
+                    ]
+                  ],
+                ),
+              );
+            },
+          ),
+        ));
   }
 }
