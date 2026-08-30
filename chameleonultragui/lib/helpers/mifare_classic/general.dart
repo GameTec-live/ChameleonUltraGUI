f78adbb111133f445792f6768924ad388e2e49e1
Kerry Shen <47771243+shezik@users.noreply.github.com>
2026-08-30 19:29:06 +0800

    fix: mfClassicPatchFirstBlock possible RangeError

--- chameleonultragui/lib/helpers/mifare_classic/general.dart
+++ chameleonultragui/lib/helpers/mifare_classic/general.dart
@@ -1,5 +1,6 @@
 import 'dart:isolate';
 import 'dart:typed_data';
+import 'dart:math' show min;
 
 import 'package:chameleonultragui/bridge/chameleon.dart';
 import 'package:chameleonultragui/generated/i18n/app_localizations.dart';
@@ -583,9 +584,11 @@ Uint8List mfClassicGenerateFirstBlock(Uint8List uid, int sak, Uint8List atqa) {
 }
 
 // reference: https://github.com/RfidResearchGroup/proxmark3/blob/master/doc/magic_cards_notes.md#mifare-classic-block0
+// normalizes input block to 16 bytes long
 Uint8List mfClassicPatchFirstBlock(
     Uint8List originalBlock0, Uint8List uid, int sak, Uint8List atqa) {
-  final block0 = Uint8List.fromList(originalBlock0);
+  final block0 = Uint8List(16);
+  block0.setRange(0, min(originalBlock0.length, block0.length), originalBlock0);
   if (uid.length == 4) {
     block0.setAll(0, uid);
     block0[4] = calculateBcc(uid);
